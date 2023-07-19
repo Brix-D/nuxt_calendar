737fdf535df109cddfdb7b37b9136f47d9ad2c3a
Brix-D <Shlyakhta4@gmail.com>
2023-07-19 22:46:33 +0400

    fix(project): project init

---
+++ nuxt.config.ts
@@ -0,0 +1,49 @@
+import { defineNuxtConfig } from 'nuxt/config'
+import { resolve } from 'node:path';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: ['@pinia/nuxt'],
+  
+  app: {
+    baseURL: '/',
+  },
+
+  css: [
+    "@/node_modules/ress/dist/ress.min.css",
+    "@/assets/scss/main.scss"
+  ],
+
+  
+  devServer: {
+    port: parseInt(process.env.DEV_PORT),
+    https: false,
+  },
+
+  
+
+  runtimeConfig: {
+    public: {
+    },
+  },
+
+  vite: {
+    plugins: [
+    ],
+    esbuild: { drop: !isDev ? ['console', 'debugger'] : [] },
+
+    css: {
+      preprocessorOptions: {
+          scss: {
+              additionalData: `
+              @import "@/assets/scss/_vars.scss";
+              `,
+          },
+      },
+  },
+  },
+})
