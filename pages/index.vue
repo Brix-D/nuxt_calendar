51f3300f16b6f3a0183e6bc2be1766799d2eb4d1
Brix-D <Shlyakhta4@gmail.com>
2023-07-20 21:02:22 +0400

    fix(calendar): start calendar

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
-        test
-        <u-icon name="logo" :size="40"/>
+        <!-- test
+        <u-icon name="logo" :size="40"/> -->
+        <Calendar />
     </div>
 </template>
 
 <script setup lang="ts">
 import UIcon  from '@/components/ui/icons/UIcon/index.vue';
-
+import Calendar from '@/components/blocks/Calendar/index.vue';
 
 </script>
 
